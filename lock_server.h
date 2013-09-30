bcac32b402ba8d254e925639cf4ed4ac97f99f3b
johnnywsd <shoudaw@gmail.com>
2013-09-29 21:58:21 -0400

    lab1

--- lock_server.h
+++ lock_server.h
@@ -8,16 +8,26 @@
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+#include <map>
+#include <pthread.h>
+
 
 class lock_server {
 
  protected:
   int nacquire;
+  std::map<lock_protocol::lockid_t, int> lock_map;
+  std::map<lock_protocol::lockid_t, pthread_cond_t*> cond_map;
+  pthread_mutex_t mutex_lock_map;
+  pthread_mutex_t mutex_acquire;
 
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+
 };
 
 #endif 
