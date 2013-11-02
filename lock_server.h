5789483af68519b9be785910e5fae1880c80bc26
johnnywsd <shoudaw@gmail.com>
2013-11-02 00:11:18 -0400

    change to my own lock_server.{cc.hh} make other xx-last

--- lock_server.h
+++ lock_server.h
@@ -1,3 +1,6 @@
+// this is the lock server
+// the lock client has a similar interface
+
 #ifndef lock_server_h
 #define lock_server_h
 
@@ -8,24 +11,23 @@
 #include <map>
 #include <pthread.h>
 
+
 class lock_server {
 
-    protected:
-        int nacquire;
-        enum lock_state { LOCKFREE, LOCKED };
-        typedef int lock_state;
-        typedef std::map<lock_protocol::lockid_t, lock_state> LockMapT;
-        LockMapT lock_map;
-        pthread_mutex_t lock_map_mutex;
-        pthread_cond_t lock_map_cond;
+ protected:
+  int nacquire;
+  std::map<lock_protocol::lockid_t, int> lock_map;
+  std::map<lock_protocol::lockid_t, pthread_cond_t*> cond_map;
+  pthread_mutex_t mutex_lock_map;
+  pthread_mutex_t mutex_acquire;
+  pthread_cond_t cond_lock_map;
 
-
-    public:
-        lock_server();
-        ~lock_server();
-        lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
-        lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
-        lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+ public:
+  lock_server();
+  ~lock_server();
+  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 
 };
 
